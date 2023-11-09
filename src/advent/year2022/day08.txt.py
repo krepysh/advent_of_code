3ec9a52339d74e8880048107114d90525547c453
Anton Topchiy <topchiy.anton@gmail.com>
2023-11-09 16:35:00 +0100

    Refactor day 8

--- src/advent/year2022/day08.txt.py
+++ src/advent/year2022/day08.txt.py
@@ -1,6 +1,5 @@
 from advent import read_input
 
-
 lines = read_input('day08.txt')
 trees = []
 for l in lines:
@@ -9,65 +8,46 @@ for l in lines:
         trees[-1].append(int(tree))
 
 
-
-H = len(lines)
-W = len(lines[0])
-
 def is_visible(height, width):
     val = trees[height][width]
     highest_left = all([t < val for t in trees[height][0:width]])
-    highest_right = all([t < val for t in trees[height][width+1:]])
+    highest_right = all([t < val for t in trees[height][width + 1:]])
     col = [trees[i][width] for i in range(len(trees))]
     highest_top = all([t < val for t in col[0:height]])
-    highest_bottom = all([t < val for t in col[height+1:]])
+    highest_bottom = all([t < val for t in col[height + 1:]])
     return any([highest_bottom, highest_top, highest_right, highest_left])
 
+
+# part 1
 r = 0
 for i in range(len(trees)):
     for j in range(len(trees[0])):
         r += is_visible(i, j)
-
 print(r)
 
-scores = []
 
-
-def vd_horizontal(i, j):
+def partial_score(row, pos):
     vd_left = 0
     vd_right = 0
-    row = trees[i]
-    val = row[j]
-    for k in range(j+1, len(row)):
+    val = row[pos]
+    for k in range(pos + 1, len(row)):
         vd_right += 1
         if row[k] >= val:
             break
 
-    for k in range(j-1, -1, -1):
+    for k in range(pos - 1, -1, -1):
         vd_left += 1
         if row[k] >= val:
             break
     return vd_left * vd_right
 
 
-def vd_vertical(i, j):
-    vd_top, vd_bottom = 0, 0
-    col = [trees[k][j] for k in range(len(trees))]
-    val = col[i]
-    for k in range(i+1, len(col)):
-        vd_top += 1
-        if col[k] >= val:
-            break
-
-    for k in range(i-1, -1, -1):
-        vd_bottom += 1
-        if col[k] >= val:
-            break
-    return vd_top * vd_bottom
-
-
-for i in range(1, len(trees)-1):
-    for j in range(1, len(trees[0])-1):
-        score = vd_horizontal(i, j) * vd_vertical(i, j)
+# part 2
+scores = []
+for i in range(1, len(trees) - 1):
+    for j in range(1, len(trees[0]) - 1):
+        col = [trees[k][j] for k in range(len(trees))]
+        score = partial_score(trees[i], j) * partial_score(col, i)
         scores.append(score)
 
 print(max(scores))
