7636335f5acaa6008a8c640acc15122ea61d2d06
Anton Topchiy <topchiy.anton@gmail.com>
2023-11-11 12:03:04 +0100

    Day 10: part 2

--- src/advent/year2022/day10.py
+++ src/advent/year2022/day10.py
@@ -1,17 +1,27 @@
 from advent import read_input
 
 
-operations = read_input('day10.txt')
+lines = read_input('day10.txt')
 # Stack of commands
-operations = operations[::-1]
+operations = lines[::-1]
 
 reper = [20, 60, 100, 140, 180, 220]
 
 acc = 0
 x = 1
 i = 0
+current_row = []
+rows = []
 while operations:
+    # part 2
+    beam_pos = (i) % 40
+    pixel = '#' if beam_pos - 1 <= x <= beam_pos + 1 else '.'
+    current_row.append(pixel)
+    if len(current_row) > 39:
+        rows.append(current_row)
+        current_row = []
     i += 1
+    # part 1
     if i in reper:
         acc += x * i
     cmd = operations.pop()
@@ -27,3 +37,6 @@ while operations:
         x += int(val)
 
 print(acc)
+
+for row in rows:
+    print(''.join(row))
