468bb39cd6998edaed1383d583db14da63112cbb
Anton Topchiy <topchiy.anton@gmail.com>
2023-11-12 13:29:04 +0100

    Day 13: part 2

--- src/advent/year2022/day13.py
+++ src/advent/year2022/day13.py
@@ -27,9 +27,28 @@ def check_signals(s1, s2):
         return len(s2) >= len(s1)
 
 
+signals = []
 r = 0
 for n, (s1, s2) in enumerate(signal_pairs):
     s1, s2 = eval(s1), eval(s2)
+    signals.append(s1)
+    signals.append(s2)
     print(n + 1, check_signals(s1, s2))
     r += (n + 1) * check_signals(s1, s2)
 print('Part 1 sum:', r)
+
+
+# part 2
+def count_pos(s):
+    pos = 0
+    for s2 in signals:
+        if s != s2:
+            pos += check_signals(s, s2)
+    return pos
+
+
+sorted_signals = sorted(signals, key=count_pos, reverse=True)
+divider1 = sorted_signals.index([[2]]) + 1
+divider2 = sorted_signals.index([[6]]) + 1
+print("Part 2 positions:", divider2, divider1)
+print("Part 2 multiplication:", divider2 * divider1)
