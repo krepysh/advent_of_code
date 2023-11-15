b95a868f37254a9bcdab0c51a37338d80222c3cc
Anton Topchiy <topchiy.anton@gmail.com>
2023-11-15 11:29:14 +0100

    Day 18: part 2

--- src/advent/year2022/day18.py
+++ src/advent/year2022/day18.py
@@ -19,13 +19,14 @@ def fill_droplet(droplet, coord):
     droplet[x][y][z] = True
 
 
-def count_neighbours(in_droplet, coord, air):
+def count_neighbours(in_droplet, coord, air=None):
     n = 0
     x, y, z = coord
     neib_coords = (x - 1, y, z), (x + 1, y, z), (x, y - 1, z), (x, y + 1, z), (x, y, z - 1), (x, y, z + 1)
     for nx, ny, nz in neib_coords:
         try:
-            val = in_droplet[nx][ny][nz] or (nx, ny, nz) not in air
+            is_bubble = ((nx, ny, nz) not in air) if air else False
+            val = in_droplet[nx][ny][nz] or is_bubble
         except IndexError:
             val = 0
         n += val
@@ -38,6 +39,7 @@ for c in lava:
 start = (19, 19, 19)
 to_visit = [start]
 while to_visit:
+    # traverse accross cube, and add to variable visited all coords accessible from start
     current = to_visit.pop()
     if current in visited:
         continue
@@ -52,7 +54,10 @@ while to_visit:
             pass
     visited.add(current)
 
+sum_of_neib_count_with_bubbles = sum([count_neighbours(lava_droplet, c) for c in lava])
 sum_of_neib_count = sum([count_neighbours(lava_droplet, c, visited) for c in lava])
 
-res = len(lava) * 6 - sum_of_neib_count
-print(res)
+part1 = len(lava) * 6 - sum_of_neib_count_with_bubbles
+part2 = len(lava) * 6 - sum_of_neib_count
+print(part1)
+print(part2)
