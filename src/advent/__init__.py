74e6137cf281c2c14c56c6007f98831d30960e95
Anton Topchiy <topchiy.anton@gmail.com>
2023-11-23 22:59:26 +0100

    Day 20: part 1

--- src/advent/__init__.py
+++ src/advent/__init__.py
@@ -1,8 +1,13 @@
-def read_input(fname: str, by_group=False) -> list[str]:
+from typing import Callable, Optional
+
+
+def read_input(fname: str, by_group=False, convert_to: Optional[Callable] = None) -> list[str]:
+    def convert(some):
+        return convert_to(some) if convert_to else some
     filename = fname
     with open(filename, 'r') as f:
         lines = f.readlines()
-    lines = [l.strip() for l in lines]
+    lines = [convert(l.strip()) for l in lines]
     if by_group:
         rv = [[]]
         for line in lines:
