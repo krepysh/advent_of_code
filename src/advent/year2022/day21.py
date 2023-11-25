cd19ad61433d8d4854b22c773f1e8aba207b0ff6
Anton Topchiy <topchiy.anton@gmail.com>
2023-11-25 10:26:04 +0100

    Day 21: part 2

--- src/advent/year2022/day21.py
+++ src/advent/year2022/day21.py
@@ -10,6 +10,8 @@ for line in lines:
         expression = int(expression)
     monkeys[monkey_name] = expression
 
+bin_ops = {'+': operator.add, '-': operator.sub, '/': operator.truediv, '*': operator.mul}
+
 
 def evaluate_expression(expression: str | int):
     if not isinstance(expression, str):
@@ -17,9 +19,34 @@ def evaluate_expression(expression: str | int):
     left_name, op, right_name = expression.split()
     left = evaluate_expression(monkeys[left_name])
     right = evaluate_expression(monkeys[right_name])
-    bin_ops = {'+': operator.add, '-': operator.sub, '/': operator.truediv, '*': operator.mul}
     return bin_ops[op](left, right)
 
 
 # Part 1
 print(evaluate_expression(monkeys['root']))
+
+
+# Part 2
+import sympy
+
+humn = sympy.Symbol('x')
+
+done = {'humn': humn}
+
+todo = [(k, v) for k, v in monkeys.items()]
+for monkey_name, expr in todo:
+    if monkey_name in done:
+        continue
+    if not isinstance(expr, str):
+        done[monkey_name] = sympy.Integer(expr)
+        continue
+    left, op, right = expr.split()
+    if left in done and right in done:
+        if monkey_name == 'root':
+            print(sympy.solve(done[left] - done[right])[0])
+            break
+        done[monkey_name] = bin_ops[op](done[left], done[right])
+    else:
+        # As we don't have values for monkeys in expr, try to process it later
+        todo.append((monkey_name, expr))
+
