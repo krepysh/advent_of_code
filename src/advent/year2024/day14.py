329b9ef446142d2b78831970266aff560e2e5f53
Anton Topchiy <topchiy.anton@gmail.com>
2024-12-14 18:27:30 +0100

    Day 14: part 2

--- src/advent/year2024/day14.py
+++ src/advent/year2024/day14.py
@@ -4,36 +4,47 @@ robots = []
 STEPS = 100
 H = 103
 L = 101
-quadrants = [0, 0, 0, 0]
+X_MEDIUM = (L - 1) // 2
+Y_MEDIUM = (H - 1) // 2
 for line in open(0).readlines():
     pos_x, pos_y, vel_x, vel_y = map(int, re.findall(r"-?\d+", line))
     robots.append((pos_x, pos_y, vel_x, vel_y,))
 results = []
-for robot in robots:
-    posx, posy, velx, vely = robot
-    posx = (posx + velx * STEPS) % L
-    posy = (posy + vely * STEPS) % H
-    results.append((posx, posy,))
 
-X_MEDIUM = (L - 1) // 2
-Y_MEDIUM = (H - 1) // 2
-print(X_MEDIUM, Y_MEDIUM)
-for posx, posy in results:
-    if posx == X_MEDIUM or posy == Y_MEDIUM:
-        continue
-    elif posx < X_MEDIUM:
-        if posy < Y_MEDIUM:
-            quadrants[0] += 1
-        else:
-            quadrants[2] += 1
-    else:
-        if posy < Y_MEDIUM:
-            quadrants[1] += 1
-        else:
-            quadrants[3] += 1
 
-print(quadrants)
-r = 1
-for q in quadrants:
-    r = q * r
-print(r)
+def print_grid(results):
+    grid = [[0]*L for _ in range(H)]
+    for x, y in results:
+        grid[y][x] += 1
+    for row in grid:
+        print("".join([str(c) if c != 0 else "." for c in row]))
+
+min_factor = float("inf")
+best_iteration = None
+
+for s in range(10000):
+    quadrants = [0, 0, 0, 0]
+    results = []
+    for robot in robots:
+        posx, posy, velx, vely = robot
+        posx = (posx + velx * s) % L
+        posy = (posy + vely * s) % H
+        results.append((posx, posy,))
+        if posx == X_MEDIUM or posy == Y_MEDIUM:
+            continue
+        elif posx < X_MEDIUM:
+            if posy < Y_MEDIUM:
+                quadrants[0] += 1
+            else:
+                quadrants[2] += 1
+        else:
+            if posy < Y_MEDIUM:
+                quadrants[1] += 1
+            else:
+                quadrants[3] += 1
+    safety_factor = quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]
+    if safety_factor < min_factor:
+        min_factor = safety_factor
+        best_iteration = s
+        print(s, quadrants)
+        print_grid(results)
