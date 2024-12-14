560ab6bfca3c7b8046e602e52cccbd8cc28fd64a
Anton Topchiy <topchiy.anton@gmail.com>
2024-12-14 18:54:38 +0100

    Day 7, both parts

---
+++ src/advent/year2024/day07.py
@@ -0,0 +1,19 @@
+targets, equations = [], []
+for line in open(0).readlines():
+    target, equation = line.split(": ")
+    targets.append(int(target))
+    equations.append(list(map(int, equation.split())))
+
+
+def is_feasible(target, equation: list[int]):
+    if len(equation) == 1:
+        return equation[0] == target
+    first, second = equation[:2]
+    return is_feasible(target, [first + second] +  equation[2::]) or is_feasible(target, [first * second] + equation[2::]) or is_feasible(target, [int(str(first) + str(second))] + equation[2::])
+
+
+res = 0
+for target, equation in zip(targets, equations):
+    res += target * is_feasible(target, equation)
+
+print(res)
