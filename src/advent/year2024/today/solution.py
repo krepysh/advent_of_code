ddec0c0ba7a8401670b130910f89c6271355df38
Anton Topchiy <topchiy.anton@gmail.com>
2024-12-22 22:16:01 +0100

    Day 22: part 2

--- src/advent/year2024/today/solution.py
+++ src/advent/year2024/today/solution.py
@@ -1,19 +1,33 @@
 base = 16777216
 
 
-def n_random(start, n):
+def random(start):
     secret = start
-    for i in range(n):
-        secret = secret ^ (secret * 64)
-        secret = secret % base
-        secret = secret ^ (secret // 32)
-        secret = secret % base
-        secret = secret ^ (secret * 2048)
-        secret = secret % base
+    secret = secret ^ (secret * 64)
+    secret = secret % base
+    secret = secret ^ (secret // 32)
+    secret = secret % base
+    secret = secret ^ (secret * 2048)
+    secret = secret % base
     return secret
 
 
 res = 0
-for start in map(int, open(0).read().split()):
-    res += (n_random(start, 2000))
-print(res)
+pattern_to_profit = {}
+for start in open(0):
+    start = int(start)
+    monkey = [start % 10]
+    seen = set()
+    for i in range(2000):
+        start = random(start)
+        monkey.append(start % 10)
+        if len(monkey) >= 5:
+            a, b, c, d, e = monkey
+            monkey.pop(0)
+            diffs = (b - a, c - b, d - c, d - e)
+            if diffs in seen:
+                continue
+            seen.add(diffs)
+            pattern_to_profit[diffs] = pattern_to_profit.get(diffs, 0) + e
+
+print(max(pattern_to_profit.items(), key=lambda x: x[1]))
