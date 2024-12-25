c0dd3cedc6d7560aa3d6a0efe430bf33bba1891f
Anton Topchiy <topchiy.anton@gmail.com>
2024-12-25 15:19:08 +0100

    Day 12, second part

--- src/advent/year2024/day12/solution.py
+++ src/advent/year2024/day12/solution.py
@@ -1,43 +1,84 @@
 from collections import deque
 
-grid = open(0).read().splitlines()
+grid = open(0).read().split()
 
-regions = []
-seen = set()
 rows = len(grid)
-cols = len(grid)
+cols = len(grid[0])
 
-for r_start in range(rows):
-    for c_start in range(cols):
-        region = []
-        q = deque()
-        q.append((r_start, c_start))
-        char = grid[r_start][c_start]
-        while q:
-            r, c = q.pop()
-            if (r, c,) in seen: continue
-            region.append((r, c,))
-            seen.add((r, c,))
-            for new_r, new_c in ((r + 1, c,), (r - 1, c,), (r, c + 1,), (r, c - 1)):
-                if 0 <= new_r < rows and 0 <= new_c < cols:
-                    if (new_r, new_c,) in seen:
-                        continue
-                    if grid[new_r][new_c] == char:
-                        q.append((new_r, new_c))
-        if region:
-            regions.append(region)
+areas = []
+seen = set()
 
 
-def get_region_perimetr(region: list[tuple[int, int]]):
-    perimetr = 4 * len(region)
-    for r, c in region:
-        for nr, nc in ((r + 1, c,), (r - 1, c,), (r, c + 1,), (r, c - 1)):
-            if (nr, nc) in region:
-                perimetr -= 1
-    return perimetr
+def get_area(start_r, start_c):
+    q = deque()
+    q.append((start_r, start_c))
+    char = grid[start_r][start_c]
+    area = []
+    while q:
+        r, c = q.pop()
+        if (r, c) in seen: continue
+        area.append((r, c))
+        seen.add((r, c))
+        for nr, nc in ((r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)):
+            if 0 <= nr < rows and 0 <= nc < cols:
+                if grid[nr][nc] == char:
+                    q.append((nr, nc))
+    return area
 
 
-total = 0
-for r in regions:
-    total += len(r) * get_region_perimetr(r)
-print(total)
+for r in range(rows):
+    for c in range(cols):
+        if (r, c) not in seen:
+            areas.append(get_area(r, c))
+
+
+def get_price(area: list[tuple[int, int]]):
+    square = len(area)
+    perimeter = 4 * square
+    for r, c in area:
+        for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
+            if 0 <= nr < rows and 0 <= nc < cols:
+                if grid[r][c] == grid[nr][nc]:
+                    perimeter -= 1
+    return square * perimeter
+
+
+def count_edges(area: list[tuple[int, int]]):
+    edges = 0
+
+    for r, c in area:
+        north_n = r - 1, c
+        west_n = r, c - 1
+        north_west_n = r - 1, c - 1
+        if north_n not in area:
+            same_edge = (west_n in area) and (north_west_n not in area)
+            if not same_edge:
+                edges += 1
+        south_n = r + 1, c
+        south_west_n = r + 1, c - 1
+
+        if south_n not in area:
+            same_edge = west_n in area and south_west_n not in area
+            if not same_edge:
+                edges += 1
+
+        if west_n not in area:
+            same_edge = (north_n in area) and  north_west_n not in area
+            if not same_edge:
+                edges += 1
+
+        east_n = r, c + 1
+        north_east_n = r -1, c + 1
+        if east_n not in area:
+            same_edge = (north_n in area) and north_east_n not in area
+            if not same_edge:
+                edges += 1
+
+    return edges
+
+
+r = 0
+for a in areas:
+    r += count_edges(a) * len(a)
+
+print(r)
